7da915805c397848d9f28b5172a3f869e84cc304
Rafael Gonzalez <astrorafael@yahoo.es>
2023-11-08 09:08:17 +0100

    initial release

--- src/gonetutils/_version.py
+++ src/gonetutils/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev0+d20231107'
-__version_tuple__ = version_tuple = (0, 1, 'dev0', 'd20231107')
+__version__ = version = '1.0.1.dev0+gff28bde.d20231108'
+__version_tuple__ = version_tuple = (1, 0, 1, 'dev0', 'gff28bde.d20231108')
