2dadf49153fd7b6f80fe9c9061c70b9985f21d08
Rafael Gonzalez <astrorafael@yahoo.es>
2023-12-19 08:12:59 +0100

    increadsed number of flats

--- scripts/flats.sh
+++ scripts/flats.sh
@@ -3,7 +3,7 @@
 read -p "Max Exposure time [ms], , hit CTRL+C to stop:: "  msecs
 read -p "Analog Gain [1..16], , hit CTRL+C to stop:: "  analoggain
 
-N=36
+N=100
 MAX=$(( msecs*1000 ))
 MIN=1000
 STEP=$(( ($MAX-$MIN)/$N ))
@@ -13,11 +13,11 @@ for (( i=0; i<=$N; i++ ))
 do
    IMAGE=flat_${i}_a_${EXPTIME}_g${analoggain}.jpg
    echo "raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${IMAGE}"
-   #raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${IMAGE} || exit 255
+   raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${IMAGE} || exit 255
 
    IMAGE=flat_${i}_b_${EXPTIME}_g${analoggain}.jpg
    echo "raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${IMAGE}"
-   #raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${IMAGE} || exit 255
+   raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${IMAGE} || exit 255
    EXPTIME=$(( $EXPTIME + $STEP ))
   
 done
