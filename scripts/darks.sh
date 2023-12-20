6b38b28539216ec67434e2319effd938d68148a2
Rafael Gonzalez <astrorafael@yahoo.es>
2023-12-20 08:55:57 +0100

    fixed dark script

--- scripts/darks.sh
+++ scripts/darks.sh
@@ -8,8 +8,8 @@ DATE=$(date +%Y%m%dT%H%M%S)
 # take a pair of darks
 FILE=dark_a_g${analoggain}.jpg
 echo "raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${FILE}"
-#raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${FILE} || exit 255
+raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${FILE} || exit 255
 
 FILE=dark_b_g${analoggain}.jpg
 echo "raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${FILE}"
-#raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${FILE} || exit 255
+raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${FILE} || exit 255
