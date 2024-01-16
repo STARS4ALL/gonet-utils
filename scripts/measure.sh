70cb26e8b8bf76999aaa89601a0a00e731e47394
Rafael Gonzalez <astrorafael@yahoo.es>
2024-01-16 11:19:33 +0100

    fixed script bug

--- scripts/measure.sh
+++ scripts/measure.sh
@@ -11,8 +11,8 @@ for (( ; ; ))
 do
    echo "raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain ${analoggain} --digitalgain 1 --output ${IMAGE}"
    raspistill --raw  --shutter ${EXPTIME} --timeout 100 -drc off --nopreview -ex off -awb off --analoggain 4 --digitalgain 1 --output ${IMAGE} || exit 255
-   echo "gonet-stats -i ${IMAGE} -wi ${width} -he ${height}"
-   gonet-stats -i ${IMAGE} -wi ${width} -he ${height}
+   echo "gonet-stats --console -i ${IMAGE} -wi ${width} -he ${height}"
+   gonet-stats --console -i ${IMAGE} -wi ${width} -he ${height}
    sleep 2
 done
 #nohup raspistill -n -t 0 -drc off -ss 600000 -ex off -awb off -ag 4 -dg 1 -r -s -o /home/pi/test.jpg > /dev/null 2>&1 &
